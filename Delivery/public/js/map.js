679bbc49ca83f5f72392c6b623d0af001bc3dd4c
Gaëtan <gaetan.chaugny@epitech.eu>
2020-05-06 18:07:40 +0200

    [+] map.js -> fix bug in fileName

--- Delivery/public/js/map.js
+++ Delivery/public/js/map.js
@@ -107,18 +107,22 @@ function interactiveMap(csv_data) {
 function getFileName() {
     var fileName;
     let date_ob = new Date();
-
+    
     let date = ("0" + date_ob.getDate()).slice(-2);
     let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
     let year = date_ob.getFullYear();
     let hour = date_ob.getHours();
 
-    if (hour > 2)
-        date = date - 1;
-    else
+    if (hour > 2) {
+	s_date = date;
+	s_date = s_date - 1;
+	date = "0" + s_date;
+    } else
         date = date - 2;
 
+    console.log(date);
     fileName = month + "-" + date + "-" + year + ".csv";
+    console.log(fileName);
     return (fileName);
 }
 
